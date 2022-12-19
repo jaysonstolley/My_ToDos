b0470c9f5e74908e8659361631bf38568795829e
jaysonstolley <66034661+jaysonstolley@users.noreply.github.com>
2022-12-18 16:03:06 -0800

    20221218 new Buttons added

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ input_box = sg.InputText(tooltip="Enter a To-Do", key='todo')
 
 add_button = sg.Button("add")
 
-list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+list_box = sg.Listbox(values=functions.get_todos(), key='todos.txt',
                       enable_events=True, size=(46, 10))
 edit_button = sg.Button('edit')
+complete_button = sg.Button("complete")
+end_button = sg.Button("end")
 
 window = sg.Window('My To-Do app',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button],
+                           [complete_button, end_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
     print(1, event)
     print(2, values)
-    print(3, values['todos'])
+    print(3, values['todos.txt'])
 
     match event:
         case 'add':
@@ -28,19 +31,26 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value=values['todos'][0])
+            window['todos.txt'].update(values=todos)
         case 'edit':
-            todo_to_edit = values['todos'][0]
+            todo_to_edit = values['todos.txt'][0]
             new_todo = values['todo']
 
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case('todos'):
-            window['todo'].update(value=values['todos'][0])
+            window['todos.txt'].update(values=todos)
+        case 'complete':
+            todo_to_complete = values['todos.txt'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos.txt'].update(values=todos)
+        case 'end':
+            break
+        case('todos.txt'):
+            window['todo'].update(value=values['todos.txt'][0])
         case sg.WINDOW_CLOSED:
             break
 window.close()
